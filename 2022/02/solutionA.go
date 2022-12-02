753916d654029c2414858b053a27101468c52763
josh kennedy <josh@outthinkgroup.com>
2022-12-02 13:41:41 -0500

    refactors to one Game type

--- 2022/02/solutionA.go
+++ 2022/02/solutionA.go
@@ -13,99 +13,11 @@ func SolutionA(){
 	totalScore := 0
 	for _, line := range data{
 		inputs := strings.Split(line," ")
-		game := NewGameA(inputs[0],inputs[1])
-		game.Play()
+		game := NewGame(inputs[0],&inputs[1], nil)
+		game.FindOutcome()
 		totalScore += game.GetScore()
 	}
 	fmt.Println("solution to A:")
 	fmt.Println(totalScore)
 }
-
-func NewGameA (o,m string)*GameA{
-	return &GameA{
-		OChoice:o,
-		MChoice:m,
-	}
-}
-
-type GameA struct {
-	OChoice string
-	MChoice string
-	Outcome int  
-	Score int
-}
-
-func (g *GameA) Play (){
-	if g.OChoice == OPAPER 	{
-		if g.MChoice == MPAPER{
-			g.Outcome = DRAW
-		}
-		if g.MChoice == MSCISSORS {
-			g.Outcome = WIN
-		}
-		if g.MChoice == MROCK {
-			g.Outcome = LOSS
-		}
-	}
-
-	if g.OChoice == OROCK 	{
-		if g.MChoice == MPAPER{
-			g.Outcome = WIN
-		}
-		if g.MChoice == MSCISSORS {
-			g.Outcome = LOSS
-		}
-		if g.MChoice == MROCK {
-			g.Outcome = DRAW
-		}
-	}
-
-	if g.OChoice == OSCISSORS {
-		if g.MChoice == MPAPER{
-			g.Outcome = LOSS
-		}
-		if g.MChoice == MSCISSORS {
-			g.Outcome = DRAW
-		}
-		if g.MChoice == MROCK {
-			g.Outcome = WIN
-		}
-	}
-}
-
-func (g *GameA) GetScore() int{
-	g.Score = g.Outcome	
-	g.Score += g.GetChoiceScore()
-	return g.Score
-}
-
-func (g *GameA) GetChoiceScore() int{
-	choice := g.MChoice
-	for _,c := range Choices {
-		if c.Kind == choice {
-			return c.Value
-		}	
-	}
-	panic("choice not found")
-}
-
-type Choice_A struct{
-	Kind string;
-	Value int;	
-}
-
-var Paper Choice_A = Choice_A{
-	Kind:MPAPER,
-	Value:2,
-}
-var Rock Choice_A = Choice_A{
-	Kind:MROCK,
-	Value:1,
-}
-var Scissors Choice_A = Choice_A{
-	Kind:MSCISSORS,
-	Value:3,
-}
-
-var Choices = []Choice_A{Scissors, Rock, Paper}
 
--- 2022/02/utils.go
+++ 2022/02/utils.go
@@ -29,14 +29,157 @@ const (
 	MPAPER = "Y"
 	MSCISSORS = "Z"
 
+	OUTCOME_LOSS = "X"
+	OUTCOME_DRAW = "Y"
+	OUTCOME_WIN = "Z"
+
 	LOSS = 0
 	DRAW = 3
 	WIN = 6
 
-	OUTCOM_LOSS = "X"
-	OUTCOM_DRAW = "Y"
-	OUTCOM_WIN = "Z"
 )
 
 
+
+func NewGame (o string, m, outcome *string)*Game{
+	if m != nil {
+
+	return &Game{
+		OChoice:o,
+		MChoice:*m,
+	}
+
+	}
+	return &Game{
+		OChoice:o,
+		NeedOutcome:*outcome,
+	}
+}
+
+type Game struct {
+	OChoice string
+	MChoice string
+	NeedOutcome string
+	Outcome int  
+	Score int
+}
+
+func (g *Game) Play(){
+	if(g.MChoice == ""){
+		g.FindChoice()
+	}
+	g.FindOutcome()
+}
+
+func (g *Game) FindChoice(){
+	if g.OChoice == OPAPER 	{
+		if g.NeedOutcome == OUTCOME_LOSS {
+			g.MChoice = MROCK
+		}
+		if g.NeedOutcome == OUTCOME_DRAW {
+			g.MChoice = MPAPER
+		}
+		if g.NeedOutcome == OUTCOME_WIN {
+			g.MChoice = MSCISSORS
+		}
+	}
+
+	if g.OChoice == OROCK 	{
+		if g.NeedOutcome == OUTCOME_LOSS{
+			g.MChoice = MSCISSORS
+		}
+		if g.NeedOutcome ==  OUTCOME_DRAW {
+			g.MChoice = MROCK
+		}
+		if g.NeedOutcome == OUTCOME_WIN {
+			g.MChoice = MPAPER
+		}
+	}
+
+	if g.OChoice == OSCISSORS {
+		if g.NeedOutcome == OUTCOME_LOSS{
+			g.MChoice = MPAPER
+		}
+		if g.NeedOutcome == OUTCOME_DRAW {
+			g.MChoice = MSCISSORS
+		}
+		if g.NeedOutcome == OUTCOME_WIN {
+			g.MChoice = MROCK
+		}
+	}
+}
+
+func (g *Game) FindOutcome (){
+	if g.OChoice == OPAPER 	{
+		if g.MChoice == MPAPER{
+			g.Outcome = DRAW
+		}
+		if g.MChoice == MSCISSORS {
+			g.Outcome = WIN
+		}
+		if g.MChoice == MROCK {
+			g.Outcome = LOSS
+		}
+	}
+
+	if g.OChoice == OROCK 	{
+		if g.MChoice == MPAPER{
+			g.Outcome = WIN
+		}
+		if g.MChoice == MSCISSORS {
+			g.Outcome = LOSS
+		}
+		if g.MChoice == MROCK {
+			g.Outcome = DRAW
+		}
+	}
+
+	if g.OChoice == OSCISSORS {
+		if g.MChoice == MPAPER{
+			g.Outcome = LOSS
+		}
+		if g.MChoice == MSCISSORS {
+			g.Outcome = DRAW
+		}
+		if g.MChoice == MROCK {
+			g.Outcome = WIN
+		}
+	}
+}
+
+func (g *Game) GetScore() int{
+	g.Score = g.Outcome	
+	g.Score += g.GetChoiceScore()
+	return g.Score
+}
+
+func (g *Game) GetChoiceScore() int{
+	choice := g.MChoice
+	for _,c := range Choices {
+		if c.Kind == choice {
+			return c.Value
+		}	
+	}
+	panic("choice not found")
+}
+
+type Choice struct{
+	Kind string;
+	Value int;	
+}
+
+var Paper Choice = Choice{
+	Kind:MPAPER,
+	Value:2,
+}
+var Rock Choice = Choice{
+	Kind:MROCK,
+	Value:1,
+}
+var Scissors Choice = Choice{
+	Kind:MSCISSORS,
+	Value:3,
+}
+
+var Choices = []Choice{Scissors, Rock, Paper}
 
