2fe8411a32d3958c80e92596fe9850cbd09a6486
josh kennedy <josh@outthinkgroup.com>
2022-12-04 08:15:27 -0500

    fmts

--- 2022/04/solutionA.go
+++ 2022/04/solutionA.go
@@ -2,50 +2,50 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 )
 
-func SolutionA(){
+func SolutionA() {
 	pairs, err := ReadLines("input.txt")
-	if err != nil { 
+	if err != nil {
 		panic("couldnt find input.txt")
 	}
 	dups := 0
-	for _,pair := range pairs {
-		if (hasDuplicateRange(pair))	{
+	for _, pair := range pairs {
+		if hasDuplicateRange(pair) {
 			dups++
 		}
 	}
 	fmt.Println("SolutionA:")
 	fmt.Println(dups)
-	fmt.Printf("%d = %d\n",dups, 582)
+	fmt.Printf("%d = %d\n", dups, 582)
 }
 
 func hasDuplicateRange(pairString string) bool {
 	ranges := strings.Split(pairString, ",")
-	rangeA := strings.Split(ranges[0],"-")
-	rangeB := strings.Split(ranges[1],"-")
+	rangeA := strings.Split(ranges[0], "-")
+	rangeB := strings.Split(ranges[1], "-")
 
-	aLow,err := strconv.ParseInt( rangeA[0],0, 0 )
+	aLow, err := strconv.ParseInt(rangeA[0], 0, 0)
 	if err != nil {
 		panic("couldnt conver aLow to a int")
 	}
-	aHi,err := strconv.ParseInt( rangeA[1], 0, 0 )
+	aHi, err := strconv.ParseInt(rangeA[1], 0, 0)
 	if err != nil {
 		panic("couldnt conver aHi to a int")
 	}
-	bLow,err := strconv.ParseInt( rangeB[0],0, 0 )
+	bLow, err := strconv.ParseInt(rangeB[0], 0, 0)
 	if err != nil {
 		panic("couldnt conver aLow to a int")
 	}
-	bHi,err := strconv.ParseInt( rangeB[1], 0, 0 )
+	bHi, err := strconv.ParseInt(rangeB[1], 0, 0)
 	if err != nil {
 		panic("couldnt conver aHi to a int")
 	}
 
 	if (aLow <= bLow && aHi >= bHi) || bLow <= aLow && bHi >= aHi {
-		return true	
+		return true
 	}
 
 	return false
